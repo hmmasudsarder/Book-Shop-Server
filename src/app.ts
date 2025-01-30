04a96efa9411ba1a00fa00a088e2203f92cbe338
hmmasudsarder <hmmasudsardar@gmail.com>
2025-01-30 16:21:05 +0600

    user or auth validation added successfully

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,16 @@ import express, { Request, Response } from 'express'
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorhandler';
-import notFound from './app/middlewares/notFound';
+import router from './app/routes';
+// import notFound from './app/middlewares/notFound';
 
 const app = express();
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({ origin: ['http://localhost:5173'], credentials: true, }))
+
+
+app.use('/api', router);  
 
 app.use(globalErrorHandler);
 
---
+++ src/app/routes/index.ts
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import { AuthRoutes } from '../modules/auth/auth.routes';
+import { userRoutes } from '../modules/user/user.route';
+// import { ProductRoutes } from '../modules/products/product.route';
+// import { OrderRoutes } from '../modules/order/order.router';
+
+const router = Router();
+
+const moduleRoutes = [
+  {
+    path: '/auth',
+    route: AuthRoutes,
+  },
+//   {
+//     path: '/products',
+//     route: ProductRoutes,
+//   },
+//   {
+//     path: '/orders',
+//     route: OrderRoutes,
+//   },
+  {
+    path: '/admin',
+    route: userRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
