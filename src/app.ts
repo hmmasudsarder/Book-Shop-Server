3883f3899ee17dc46dd6b6915ebdc6c998991b3b
hmmasudsarder <hmmasudsardar@gmail.com>
2025-07-10 00:55:43 +0600

    added credentials

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorhandler';
@@ -6,21 +6,28 @@ import router from './app/routes';
 // import notFound from './app/middlewares/notFound';
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ['https://book-shop-theta-bay.vercel.app', "http://localhost:5173"], credentials: true, }))
+app.use(
+  cors({
+    origin: [
+      'https://book-shop-theta-bay.vercel.app',
+      'http://localhost:5173',
+      'https://ts-book-store.vercel.app/',
+    ],
+    credentials: true,
+  })
+);
 
-
-app.use('/api', router);  
+app.use('/api', router);
 
 app.use(globalErrorHandler);
 
 //Not Found
 // app.use(notFound);
 
-
-app.get("/", (req: Request, res: Response) => {
-    res.send("server is ongoing 🏃‍♂️‍➡️☄️")
-})
+app.get('/', (req: Request, res: Response) => {
+  res.send('server is ongoing 🏃‍♂️‍➡️☄️');
+});
 
 export default app;
